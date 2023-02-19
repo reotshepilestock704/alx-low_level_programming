989f2b36008911dc64b57dac5b6f7d87cf2ce8b9
reotshepilstock704 <reotshepile704@gmail.com>
2023-02-19 11:42:41 -0800

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,13 @@
-#!/bin/bash
-int puts(Programming is like building a multilingual puzzle)
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
 
