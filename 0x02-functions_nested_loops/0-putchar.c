eb8134a5270b08c7b93889b201061ed72c8bea62
reotshepilstock704 <reotshepile704@gmail.com>
2023-02-22 15:25:47 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
 #include "main.h"
-	
 /**
  * main - Entry point
- * Return: Always 0
+ * Description: 'prints _putchar to stdout'
+ * Return: always 0
  */
-
 
 int main(void)
 {
@@ -19,4 +18,3 @@ int main(void)
 	_putchar('\n');
 	return (0);
 }
-
